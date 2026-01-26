024538735a094d868c4e16db0dbcae6d6f797893
Eric Jamieson <ericbrucejamieson@gmail.com>
2026-01-25 18:44:44 -0600

    update vignette

--- R/undidR-package.R
+++ R/undidR-package.R
@@ -5,12 +5,11 @@
 #'
 #' @details
 #' A framework for estimating difference-in-differences with unpoolable data,
-#' based on Karim, Webb, Austin, and Strumpf (2025)
-#' <https://doi.org/10.48550/arXiv.2403.15910>. Supports common or staggered
-#' adoption, multiple groups, and the inclusion of covariates. Also computes
-#' p-values for the aggregate average treatment effect on the treated via the
-#' randomization inference procedure described in MacKinnon and Webb (2020)
-#' <https://doi.org/10.1016/j.jeconom.2020.04.024>.
+#' based on Karim, Webb, Austin, and Strumpf (2025).
+#' Supports common or staggered adoption, multiple groups, and the inclusion
+#' of covariates. Also computes p-values for the aggregate average treatment
+#' effect on the treated via the randomization inference procedure described
+#' in MacKinnon and Webb (2020).
 #'
 #'
 #' @section Stage One - Initialize:
@@ -30,7 +29,6 @@
 #' }
 #'
 #' @author Eric Jamieson
-#' Maintainer: <ericbrucejamieson@@gmail.com>
 #'
 #' @keywords internal
 "_PACKAGE"
